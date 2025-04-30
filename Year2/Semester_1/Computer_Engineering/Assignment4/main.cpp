0125ec1accbb894b7e46a1bd60be209339d4ed6e
Ane O'Byrne <obyrnean@tcd.ie>
2025-04-30 19:54:27 +0200

    main.cpp

---
+++ Year2/Semester_1/Computer_Engineering/Assignment4/main.cpp
@@ -0,0 +1,16 @@
+#include "AlbumCollection.h"
+using namespace std;
+AlbumCollection db;
+void InsertAlbums(){
+    db.InsertAlbum("The Beatles", "Sgt Pepper's Lonely Hearts Club Band", 1967);
+    db.InsertAlbum("Jay-Z", "The Blueprint", 2001);
+    db.InsertAlbum("The White Stripes", "Elephant", 2003);
+    db.InsertAlbum("Dua Lipa", "Future Nostalgia", 2020);
+    db.InsertAlbum("Taylor Swift", "Red (Taylor's Version)", 2021);
+    db.InsertAlbum("The Beatles", "Yellow Submarine", 1969);
+    db.InsertAlbum("The Plagiarist", "Future Nostalgia", 2021);
+}
+int main() {
+    InsertAlbums();
+    db.PrintByYear(2021);
+}
