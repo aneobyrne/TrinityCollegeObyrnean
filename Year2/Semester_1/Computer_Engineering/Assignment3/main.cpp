7ecd9c333961a67e7b3cca68db613966560bcb1e
Ane O'Byrne <obyrnean@tcd.ie>
2025-04-30 19:45:52 +0200

    main.cpp

---
+++ Year2/Semester_1/Computer_Engineering/Assignment3/main.cpp
@@ -0,0 +1,14 @@
+#include "UnsortedWordList.h"
+
+int main() {
+    UnsortedWordList list;
+    list.CountWord("Ultimate");
+    list.CountWord("Question");
+    list.CountWord("to");
+    list.CountWord("Life");
+    list.CountWord("Universe");
+    list.CountWord("and");
+    list.CountWord("Everything");
+    list.CountWord("Question");
+    list.Print();
+}
