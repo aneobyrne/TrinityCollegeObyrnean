227d98751b7d8a12d45d1323ddf49ba68bde974b
Ane O'Byrne <obyrnean@tcd.ie>
2025-05-01 12:29:03 +0200

    bst.h

---
+++ Year3/Semester1/Data_Structures/Assignment_3/Asssignment_3.2(task1)/bst.h
@@ -0,0 +1,18 @@
+
+
+#ifndef BST_H_
+#define BST_H_
+typedef struct Tree_Node{
+    char data; //character stored in the node
+    struct Tree_Node* left; //pointer to next left node in the tree
+    struct Tree_Node* right; //pointer to next right node in the tree
+} Tree_Node;
+
+void tree_insert ( Tree_Node** root, char data );
+Tree_Node* create_bst (char data[]);
+Tree_Node* tree_search ( Tree_Node* root, char data );
+void tree_print_sorted ( Tree_Node* root );
+void tree_delete ( Tree_Node* root );
+
+#endif
+
